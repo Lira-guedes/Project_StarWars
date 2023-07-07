4e8a335026ecf2ad4aee71e7c96bcd225032d864
Lira Guedes <liracrg@gmail.com>
2023-07-07 16:15:26 -0300

    feat: requisito 1 - criando tabela com dados da api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Table from './components/Table';
+import PlanetsContext from './context/PlanetsContext';
+import fetchApi from './service/useFetch';
 
 function App() {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await fetchApi();
+      setPlanets(response);
+      console.log(response);
+    };
+    fetch();
+  }, []);
+
   return (
-    <span> SatarWars </span>
+    <PlanetsContext.Provider value={ { planets } }>
+      <Table />
+    </PlanetsContext.Provider>
   );
 }
 
---
+++ src/components/Table.js
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react';
+import PlanetsContext from '../context/PlanetsContext';
+
+function Table() {
+  const { planets } = useContext(PlanetsContext);
+  return (
+    <div>
+      <h1> StarWars Planets Search </h1>
+      <table>
+        <thead>
+          <th>
+            <th> Nome </th>
+            <th> Rotation Period </th>
+            <th> Orbital Period </th>
+            <th> Diameter </th>
+            <th> Climate </th>
+            <th> Gravity </th>
+            <th> Terrain </th>
+            <th> Surface Water </th>
+            <th> Population </th>
+            <th> Films </th>
+            <th> Created </th>
+            <th> Edited </th>
+            <th> Url </th>
+          </th>
+        </thead>
+        <tbody>
+          { planets ? planets.map((elem) => (
+            <tr key={ elem.name }>
+              <td>{ elem.name }</td>
+              <td>{ elem.rotation_period }</td>
+              <td>{ elem.orbital_period }</td>
+              <td>{ elem.diameter }</td>
+              <td>{ elem.climate }</td>
+              <td>{ elem.gravity }</td>
+              <td>{ elem.terrain }</td>
+              <td>{ elem.surface_water }</td>
+              <td>{ elem.population }</td>
+              <td>{ elem.films }</td>
+              <td>{ elem.created }</td>
+              <td>{ elem.edited }</td>
+              <td>{ elem.url }</td>
+            </tr>
+          )) : console.log('erro')}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
