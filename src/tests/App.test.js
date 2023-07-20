f969060cb44fac365760c1b8a9829931799deb35
Lira Guedes <liracrg@gmail.com>
2023-07-20 18:13:28 -0300

    test: finalizando cobertura de 100% do stesteeeeeessss

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -70,8 +70,13 @@ describe(' ', () => {
     // expect(value.value).toBe('1234567');
   });
 
-  test('Testando Filtros de Coluna Operador e Value', async () => {
+  test('Testando Filtros de Coluna Operador e Value diferentes', async () => {
     render(<App />);
+            await waitFor(() => {
+    expect(screen.getByText(/Tatooine/)).toBeInTheDocument();
+    expect(screen.getByText(/Yavin IV/)).toBeInTheDocument();
+    expect(screen.getByText(/Hoth/)).toBeInTheDocument();
+   }, { timeout: 10000 });
     const column = screen.getByTestId('column-filter');
     const comparison = screen.getByTestId('comparison-filter');
     const value = screen.getByTestId('value-filter');
@@ -102,20 +107,10 @@ describe(' ', () => {
 
     await waitFor(() => {
       expect(screen.getByText(/Tatooine/)).toBeInTheDocument();
-      expect(screen.getByText(/Yavin/)).toBeInTheDocument();
-      expect(screen.getByText(/Dagobah/)).toBeInTheDocument();
+      expect(screen.getByText(/Yavin IV/)).toBeInTheDocument();
+      expect(screen.getByText(/Naboo/)).toBeInTheDocument();
     }, { timeout: 10000 });
 
-    userEvent.clear(value)
-
-    userEvent.selectOptions(column, 'rotation_period');
-    userEvent.selectOptions(comparison, 'igual a');
-    userEvent.type(value, '27');
-    userEvent.click(button);
-
-    // await waitFor(() => {
-    //   expect(screen.getByText(/Kamino/i)).toBeInTheDocument();
-    // }, { timeout: 10000 });
   });
 
   test('Testando Filtros por Nome', async () => {
@@ -126,7 +121,6 @@ describe(' ', () => {
     userEvent.type(name, 'Bespin');
     expect(name.value).toBe('Bespin')
     await waitFor(() => {
-          expect(screen.getAllByRole('row')).toHaveLength(2);
       expect(screen.getByText(/Bespin/i)).toBeInTheDocument();
     }, { timeout: 10000 });
 
@@ -138,36 +132,38 @@ describe(' ', () => {
     }, { timeout: 10000 });
   });
 
-  // });
   test('Adiciona Filtro quando botão é clicado', async () => {
     render(<App />);
     const column = screen.getByTestId('column-filter');
     const comparison = screen.getByTestId('comparison-filter');
     const value = screen.getByTestId('value-filter');
     const buttonElement = screen.getByTestId('button-filter');
-
-    userEvent.selectOptions(column, 'population');
-    userEvent.selectOptions(comparison, 'maior que');
     userEvent.clear(value)
-    userEvent.type(value, '123456');
+
+    userEvent.selectOptions(column, 'rotation_period');
+    userEvent.selectOptions(comparison, 'menor que');
+    userEvent.type(value, '23');
     userEvent.click(buttonElement);
 
-    expect(screen.getByText(/population maior que 123456/)).toBeInTheDocument();
+    expect(screen.getByText(/rotation_period menor que 23/)).toBeInTheDocument();
     expect(screen.getAllByText(/delete/)[0]).toBeInTheDocument();
 
+    await waitFor(() => {
+      expect(screen.getByText(/Bespin/i)).toBeInTheDocument();
+      expect(screen.getByText(/Endor/i)).toBeInTheDocument();
+    }, { timeout: 10000 });
   });
 
-    test('Remove um Filtro quando botão é clicado', async () => {
+    test('Remove um tipo de Filtro quando botão é clicado', async () => {
     render(<App />);
         await waitFor(() => {
-        expect(screen.getByText(/Yavin IV/)).toBeInTheDocument();
-        expect(screen.getByText(/Hoth/)).toBeInTheDocument();   
+    expect(screen.getByText(/Yavin IV/)).toBeInTheDocument();
    }, { timeout: 10000 });
+
     const column = screen.getByTestId('column-filter');
     const comparison = screen.getByTestId('comparison-filter');
     const value = screen.getByTestId('value-filter');
     const buttonElement = screen.getByTestId('button-filter');
-    
     userEvent.selectOptions(column, 'population');
     userEvent.selectOptions(comparison, 'maior que');
     userEvent.clear(value)
@@ -176,17 +172,117 @@ describe(' ', () => {
 
     expect(screen.getByText(/population maior que 123456/)).toBeInTheDocument();
     expect(screen.getAllByText(/delete/)[0]).toBeInTheDocument();
+    expect(screen.queryByText(/Yavin IV/)).not.toBeInTheDocument();
+    expect(screen.getByText(/Naboo/)).toBeInTheDocument();
+    expect(screen.getAllByRole('row')).toHaveLength(8);
 
     const deleteBtn = screen.getByRole('button', {name: /delete/i});
     act(() => userEvent.click(deleteBtn));
 
     expect(screen.queryByText(/population maior que 123456/)).not.toBeInTheDocument();
     expect(screen.queryByText(/delete/)).not.toBeInTheDocument();
-    
+    expect(screen.getByText(/Tatooine/)).toBeInTheDocument();
     expect(screen.getByText(/Yavin IV/)).toBeInTheDocument();
-    expect(screen.getByText(/Hoth/)).toBeInTheDocument();
     expect(screen.getAllByRole('row')).toHaveLength(11);
+  });
 
+  test('Remove o Filtro quando botão é clicado', async () => {
+    render(<App />);
+        await waitFor(() => {
+        expect(screen.getByText(/Tatooine/)).toBeInTheDocument();
+        expect(screen.getByText(/Yavin IV/)).toBeInTheDocument();
+        expect(screen.getByText(/Hoth/)).toBeInTheDocument();
+   }, { timeout: 20000 });
+
+    const column = screen.getByTestId('column-filter');
+    const comparison = screen.getByTestId('comparison-filter');
+    const value = screen.getByTestId('value-filter');
+    const buttonElement = screen.getByTestId('button-filter');
+
+    // 1
+    userEvent.selectOptions(column, 'surface_water');
+    userEvent.selectOptions(comparison, 'menor que');
+    userEvent.clear(value)
+    userEvent.type(value, '10');
+    userEvent.click(buttonElement);
+
+    expect(screen.getByText(/surface_water menor que 10/)).toBeInTheDocument();
+    expect(screen.getAllByText(/delete/)[0]).toBeInTheDocument();
+    expect(screen.getByText(/Endor/)).toBeInTheDocument();
+    expect(screen.queryByText(/Tatooine/)).toBeInTheDocument();
+
+// 2
+    userEvent.selectOptions(column, 'population');
+    userEvent.selectOptions(comparison, 'maior que');
+    userEvent.clear(value)
+    userEvent.type(value, '200000');
+    userEvent.click(buttonElement); 
+
+
+    expect(screen.getByText(/surface_water menor que 10/)).toBeInTheDocument();
+    expect(screen.getAllByText(/delete/)[0]).toBeInTheDocument();
+    expect(screen.getByText(/population maior que 200000/)).toBeInTheDocument();
+    expect(screen.getAllByText(/delete/)[1]).toBeInTheDocument();
+    expect(screen.queryByText(/Bespin/)).toBeInTheDocument();
+    expect(screen.queryByText(/Endor/)).toBeInTheDocument();
+
+     //3
+    userEvent.selectOptions(column, 'diameter');
+    userEvent.selectOptions(comparison, 'igual a');
+    userEvent.clear(value)
+    userEvent.type(value, '4900');
+    userEvent.click(buttonElement); 
+
+    expect(screen.getByText(/surface_water menor que 10/)).toBeInTheDocument();
+    expect(screen.getAllByText(/delete/)[0]).toBeInTheDocument();
+    expect(screen.getByText(/population maior que 200000/)).toBeInTheDocument();
+    expect(screen.getAllByText(/delete/)[1]).toBeInTheDocument();
+    expect(screen.getByText(/diameter igual a 4900/)).toBeInTheDocument();
+    expect(screen.getAllByText(/delete/)[2]).toBeInTheDocument();
+    expect(screen.queryByText(/Endor/)).toBeInTheDocument();
+
+
+     //4
+    userEvent.selectOptions(column, 'orbital_period');
+    userEvent.selectOptions(comparison, 'igual a');
+    userEvent.clear(value)
+    userEvent.type(value, '402');
+    userEvent.click(buttonElement); 
+
+    expect(screen.getByText(/surface_water menor que 10/)).toBeInTheDocument();
+    expect(screen.getAllByText(/delete/)[0]).toBeInTheDocument();
+    expect(screen.getByText(/population maior que 200000/)).toBeInTheDocument();
+    expect(screen.getAllByText(/delete/)[1]).toBeInTheDocument();
+     expect(screen.getByText(/diameter igual a 4900/)).toBeInTheDocument();
+    expect(screen.getAllByText(/delete/)[2]).toBeInTheDocument();
+    expect(screen.getByText(/orbital_period igual a 402/)).toBeInTheDocument();
+    expect(screen.getAllByText(/delete/)[3]).toBeInTheDocument();
+    expect(screen.queryByText(/Endor/)).toBeInTheDocument();
+
+    // delete
+    const deleteBtn = screen.getAllByRole('button', {name: /delete/i});
+    act(() => userEvent.click(deleteBtn[3]));
+
+    expect(screen.getByText(/Endor/)).toBeInTheDocument();
+   expect(screen.queryByText(/orbital_period igual a 402/)).not.toBeInTheDocument();
+    expect(deleteBtn[3]).not.toBeInTheDocument();
+
+    act(() => userEvent.click(deleteBtn[2]));
+    expect(screen.getByText(/Bespin/)).toBeInTheDocument();
+     expect(screen.queryByText(/diameter igual a 4900/)).not.toBeInTheDocument();
+    expect(deleteBtn[2]).not.toBeInTheDocument();
+
+    // expect(screen.queryByText(/surface_water menor que 10/)).not.toBeInTheDocument();
+    // expect(screen.queryByText(/delete/)).not.toBeInTheDocument();
+    
+    // expect(screen.getByText(/Tatooine/)).toBeInTheDocument();
+
+    // expect(screen.getByText(/Yavin IV/)).toBeInTheDocument();
+    // expect(screen.getByText(/Hoth/)).toBeInTheDocument();
+    // expect(screen.getByText(/Dagobah/)).toBeInTheDocument();
+    // expect(screen.getByText(/Coruscant/)).toBeInTheDocument();
+    // expect(screen.getByText(/Kamino/)).toBeInTheDocument();
+    // expect(screen.getAllByRole('row')).toHaveLength(11);
   });
 
     test('Remove todos os filtros ', () => {
@@ -205,29 +301,4 @@ describe(' ', () => {
     expect(screen.getByTestId('column-filter')[3].textContent).toBe('rotation_period');
     expect(screen.getByTestId('column-filter')[4].textContent).toBe('surface_water');
   });
-
-  //   test.only('should delete a filter when the delete button is clicked', () => {
-  //   // Mock the filters array
-  //   const filters = [
-  //     { coluna: 'population', operador: 'maior que', input: 100000 },
-  //     { coluna: 'orbital_period', operador: 'menor que', input: 200 },
-  //   ];
-  //   const setFilteredPlanetsMock = jest.fn();
-
-  //   // Render the component with mock data
-  //    render(<App />);
-
-
-  //   // Select the delete button for the first filter
-  //   const deleteButton = screen.getByText('delete');
-
-  //   // Click the delete button
-  //   fireEvent.click(deleteButton);
-
-  //   // Check if the setFilteredPlanetsMock function was called with the correct filtered array
-  //   expect(setFilteredPlanetsMock).toHaveBeenCalledTimes(1);
-  //   expect(setFilteredPlanetsMock).toHaveBeenCalledWith([
-  //     { coluna: 'orbital_period', operador: 'menor que', input: 200 },
-  //   ]);
-  // });
 });
